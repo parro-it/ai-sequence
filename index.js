d546e1fe4bd2ce44ba5b5860655c79b573351a09
Andrea Parodi <andrea@parro.it>
2017-11-28 21:33:53 +0100

    first version

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-export default async function* aiSequence(data) {
-  for (const item of data) {
-    yield item;
+import AsyncIterable from "asynciterable";
+
+const iterateTo = async (write, iterable) => {
+  const generator = iterable[Symbol.asyncIterator] || iterable[Symbol.iterator];
+  const iterator = generator.call(iterable);
+  let item = await iterator.next();
+  while (!item.done) {
+    write(await item.value);
+    item = await iterator.next();
   }
+};
+
+export default function aiMerge(...sources) {
+  return new AsyncIterable(async (write, end) => {
+    for (const source of sources) {
+      await iterateTo(write, source);
+    }
+    end();
+  });
 }
--- test.js
+++ test.js
@@ -1,5 +1,22 @@
 import test from "tape-async";
 import aiSequence from ".";
+import delay from "delay";
+import concat from "ai-concat";
+
+function buildIterable(arr) {
+  return arr.map(v => delay(v * 10).then(() => v));
+}
+
+test("merge iterables in parallel", async t => {
+  const iterable = aiSequence(
+    buildIterable([3, 2, 1]),
+    buildIterable([6, 4, 5]),
+    buildIterable([10, 9, 8, 7])
+  );
+
+  const result = await concat.obj(iterable);
+  t.deepEqual([3, 2, 1, 6, 4, 5, 10, 9, 8, 7], result);
+});
 
 test("exports a function", async t => {
   t.is(typeof aiSequence, "function");
